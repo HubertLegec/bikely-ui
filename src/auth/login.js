47bc1033970c239fd450697c478f88d2ba6c86cf
kacper-cyra <kacper.cyranowski@gmail.com>
2021-03-18 22:34:15 +0100

    init

---
+++ src/auth/login.js
@@ -0,0 +1,11 @@
+import { FormControl, InputLabel, Input, FormHelperText } from "@material-ui/core";
+
+export function Login(props) {
+  return (
+    <FormControl>
+      <InputLabel htmlFor="my-input">Email address</InputLabel>
+      <Input id="my-input" aria-describedby="my-helper-text" />
+      <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+    </FormControl>
+  );
+}
