c3a35873989e4a6cd6796fec59867bd45f51d821
Mikołaj <miki87@o2.pl>
2021-04-11 19:18:05 +0200

    registerformUI-refactored

--- src/components/forms/registerForm/RegisterForm.jsx
+++ src/components/forms/registerForm/RegisterForm.jsx
@@ -1,101 +1,148 @@
 import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import { FormGroup, Button } from '@material-ui/core';
-import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+// import { useHistory } from 'react-router-dom';
 
 import { EmailInput, PasswordInput, UsernameInput } from '../inputs';
-import { validateEmail, validatePassword, validateUsername } from '../../../helpers/validation';
-import styles from '../form.module.css';
-import { BikelyApi } from '../../../api/BikelyApi';
+// import { validateEmail, validatePassword, validateUsername } from '../../../helpers/validation';
+import styles from '../form.module.css'; //czy to potrzebne?
+// import { BikelyApi } from '../../../api/BikelyApi';
 
-export const RegisterForm = () => {
-  const [inputValues, setInputValues] = useState({ email: '', password: '', username: '' });
-  const [inputErrors, setInputErrors] = useState({ email: '', password: '', username: '' });
-  const [formError, setFormError] = useState('');
-  const history = useHistory();
+import { useStyles } from './RegisterForm.styles';
 
-  const validate = (values) => {
-    const errors = inputErrors;
-    let isAllValid = true;
+export const RegisterForm = ({ formik, formError }) => {
+  const [/*formError,*/ setFormError] = useState('');
+  const classes = useStyles();
+  // const [inputValues, setInputValues] = useState({ email: '', password: '', username: '' });
+  // const [inputErrors, setInputErrors] = useState({ email: '', password: '', username: '' });
+  // const history = useHistory();
 
-    if (inputValues.email !== values.email) {
-      errors.email = validateEmail(values.email);
-    } else if (inputValues.password !== values.password) {
-      errors.password = validatePassword(values.password);
-    } else if (inputValues.username !== values.username) {
-      errors.username = validateUsername(values.username);
-    } else {
-      errors.email = validateEmail(values.email);
-      errors.password = validatePassword(values.password);
-      errors.username = validateUsername(values.username);
-    }
+  const { handleSubmit, errors, values, handleChange, isSubmitting } = formik;
+  const { password, email, username } = values; //czy username tutaj?
 
-    setInputErrors(errors);
-    setInputValues(values);
-    for (const error in errors) {
-      if (errors[error].length !== 0) isAllValid = false;
-    }
+  // const validate = (values) => {
+  //   const errors = inputErrors;
+  //   let isAllValid = true;
 
-    return isAllValid ? null : errors;
-  };
+  //   if (inputValues.email !== values.email) {
+  //     errors.email = validateEmail(values.email);
+  //   } else if (inputValues.password !== values.password) {
+  //     errors.password = validatePassword(values.password);
+  //   } else if (inputValues.username !== values.username) {
+  //     errors.username = validateUsername(values.username);
+  //   } else {
+  //     errors.email = validateEmail(values.email);
+  //     errors.password = validatePassword(values.password);
+  //     errors.username = validateUsername(values.username);
+  //   }
 
-  const onSubmit = async (values, { setSubmitting }) => {
-    setFormError('');
-    const registerResponse = await BikelyApi.register(values);
-    if (!registerResponse.error) {
-      const loginResponse = await BikelyApi.login(values);
-      if (loginResponse.error) {
-        setFormError('Something went wrong');
+  //   setInputErrors(errors);
+  //   setInputValues(values);
+  //   for (const error in errors) {
+  //     if (errors[error].length !== 0) isAllValid = false;
+  //   }
 
-        setSubmitting(false);
-      } else if (BikelyApi.userHasAuthenticated()) history.push('/');
-    } else {
-      if (registerResponse.statusCode === 400 && Array.isArray(registerResponse.message))
-        setFormError(registerResponse.message[0]);
-      setFormError('Something went wrong');
-    }
-    setFormError('Something went wrong');
+  //   return isAllValid ? null : errors;
+  // };
 
-    setSubmitting(false);
-  };
+  // const onSubmit = async (values, { setSubmitting }) => {
+  //   setFormError('');
+  //   const registerResponse = await BikelyApi.register(values);
+  //   if (!registerResponse.error) {
+  //     const loginResponse = await BikelyApi.login(values);
+  //     if (loginResponse.error) {
+  //       setFormError('Something went wrong');
 
-  const formik = useFormik({
-    initialValues: { email: '', password: '', username: '' },
-    validate,
-    onSubmit,
-  });
+  //       setSubmitting(false);
+  //     } else if (BikelyApi.userHasAuthenticated()) history.push('/');
+  //   } else {
+  //     if (registerResponse.statusCode === 400 && Array.isArray(registerResponse.message))
+  //       setFormError(registerResponse.message[0]);
+  //     setFormError('Something went wrong');
+  //   }
+  //   setFormError('Something went wrong');
+
+  //   setSubmitting(false);
+  // };
+
+  // const formik = useFormik({
+  //   initialValues: { email: '', password: '', username: '' },
+  //   validate,
+  //   onSubmit,
+  // });
 
   return (
-    <form className="register" onSubmit={formik.handleSubmit}>
+    <form className="register" onSubmit={handleSubmit}>
       <FormGroup className={styles.loginForm}>
-        <EmailInput
-          errors={formik.errors.email}
-          id="email"
-          name="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-        />
+        <EmailInput errors={errors.email} id="email" name="email" value={email} onChange={handleChange} />
         <UsernameInput
-          errors={formik.errors.username}
+          errors={errors.username}
           id="username"
           name="username"
-          value={formik.values.username}
-          onChange={formik.handleChange}
+          value={username}
+          onChange={handleChange}
         />
         <PasswordInput
-          errors={formik.errors.password}
+          errors={errors.password}
           id="password"
           name="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
+          value={password}
+          onChange={handleChange}
         />
         <div id="formError" className={styles.formError}>
           {formError}
         </div>
-        <Button type="submit" className={styles.submitButton} disabled={formik.isSubmitting}>
-          Submit
+        <Button
+          variant="outlined"
+          color="primary"
+          className={classes.submitButton}
+          type="submit"
+          disabled={isSubmitting}
+        >
+          Register
         </Button>
       </FormGroup>
     </form>
   );
 };
+
+RegisterForm.propTypes = {
+  formik: PropTypes.object,
+  formError: PropTypes.string,
+};
+
+///////kopia jako punkt odniesienia
+// return (
+//   <form className="register" onSubmit={formik.handleSubmit}>
+//     <FormGroup className={styles.loginForm}>
+//       <EmailInput
+//         errors={formik.errors.email}
+//         id="email"
+//         name="email"
+//         value={formik.values.email}
+//         onChange={formik.handleChange}
+//       />
+//       <UsernameInput
+//         errors={formik.errors.username}
+//         id="username"
+//         name="username"
+//         value={formik.values.username}
+//         onChange={formik.handleChange}
+//       />
+//       <PasswordInput
+//         errors={formik.errors.password}
+//         id="password"
+//         name="password"
+//         value={formik.values.password}
+//         onChange={formik.handleChange}
+//       />
+//       <div id="formError" className={styles.formError}>
+//         {formError}
+//       </div>
+//       <Button type="submit" className={styles.submitButton} disabled={formik.isSubmitting}>
+//         Submit
+//       </Button>
+//     </FormGroup>
+//   </form>
+// );
--- src/pages/registerPage/RegisterPage.jsx
+++ src/pages/registerPage/RegisterPage.jsx
@@ -1,20 +1,81 @@
-import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Paper } from '@material-ui/core';
+import { useFormik } from 'formik';
+import { useHistory } from 'react-router-dom';
 
 import { RegisterForm } from '../../components/forms';
+import { validateEmail, validatePassword, validateUsername } from '../../helpers/validation';
+import { BikelyApi } from '../../api/BikelyApi';
 
 import { useStyles } from './RegisterPage.styles';
 
 export const RegisterPage = () => {
   const classes = useStyles();
+  const [inputValues, setInputValues] = useState({ email: '', password: '', username: '' });
+  const [inputErrors, setInputErrors] = useState({ email: '', password: '', username: '' });
+  const [formError, setFormError] = useState('');
+  const history = useHistory();
+
+  const validate = (values) => {
+    const errors = inputErrors;
+    let isAllValid = true;
+
+    if (inputValues.email !== values.email) {
+      errors.email = validateEmail(values.email);
+    } else if (inputValues.password !== values.password) {
+      errors.password = validatePassword(values.password);
+    } else if (inputValues.username !== values.username) {
+      errors.username = validateUsername(values.username);
+    } else {
+      errors.email = validateEmail(values.email);
+      errors.password = validatePassword(values.password);
+      errors.username = validateUsername(values.username);
+    }
+
+    setInputErrors(errors);
+    setInputValues(values);
+    for (const error in errors) {
+      if (errors[error].length !== 0) isAllValid = false;
+    }
+
+    return isAllValid ? null : errors;
+  };
+
+  const onSubmit = async (values, { setSubmitting }) => {
+    setFormError('');
+    const registerResponse = await BikelyApi.register(values);
+    if (!registerResponse.error) {
+      const loginResponse = await BikelyApi.login(values);
+      if (loginResponse.error) {
+        setFormError('Something went wrong');
+
+        setSubmitting(false);
+      } else if (BikelyApi.userHasAuthenticated()) history.push('/');
+    } else {
+      if (registerResponse.statusCode === 400 && Array.isArray(registerResponse.message))
+        setFormError(registerResponse.message[0]);
+      setFormError('Something went wrong');
+    }
+    setFormError('Something went wrong');
+
+    setSubmitting(false);
+  };
+
+  const formik = useFormik({
+    initialValues: { email: '', password: '', username: '' },
+    validate,
+    onSubmit,
+  });
 
   return (
-    <Grid alignItems="center" justify="center" style={{ minHeight: '100vh' }} container spacing={0}>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>
-          <RegisterForm />
-        </Paper>
-      </Grid>
-    </Grid>
+    // <Grid alignItems="center" justify="center" style={{ minHeight: '100vh' }} container spacing={0}>
+    //   <Grid item xs={4}>
+    <div className={classes.container}>
+      <Paper className={classes.paper}>
+        <RegisterForm formik={formik} formError={formError} />
+      </Paper>
+    </div>
+    //   </Grid>
+    // </Grid>
   );
 };
